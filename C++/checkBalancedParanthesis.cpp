cefe4675c6cfadc13a470c43f2f63dfe39607e9f
PoovizhiKannan-PK <59499706+PoovizhiKannan-PK@users.noreply.github.com>
2021-02-12 01:38:52 +0530

    Add files via upload

---
+++ C++/checkBalancedParanthesis.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main(){
+    stack <char> s;
+    string str;
+    bool flag = true;
+    cout << "Input string";
+    cin >> str;
+    for(int i = 0; i < str.length(); i++){
+        if (str[i] == '{' || str[i] == '(' || str[i] == '['){
+            s.push(str[i]);
+        }
+        else if(str[i] == '}'){
+            if(s.top() == '{'){
+                s.pop();
+            }
+            else{
+                flag = false;
+                break;
+            }
+        }
+        else if(str[i] == ')'){
+            if(s.top() == '('){
+                s.pop();
+            }
+            else{
+                flag = false;
+                break;
+            }
+        }
+        else if(str[i] == ']'){
+            if(s.top() == '['){
+                s.pop();
+            }
+            else{
+                flag = false;
+                break;
+            }
+        }
+    }
+
+    if(flag == true){
+        cout << "balanced";
+    }
+    else{
+        cout << "unblalanced";
+    }
+    return 0;
+}
