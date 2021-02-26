0dc1756afa823d94c5d4352965c8680248649b85
PoovizhiKannan-PK <59499706+PoovizhiKannan-PK@users.noreply.github.com>
2021-02-26 15:05:53 +0530

    ""

--- C++ Data Strutures/Print.cpp
+++ C++ Data Strutures/Print.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
 int main(){
+
     string f_name, l_name;
-    cout << "Enter names";
+    
+    cout << "Enter names: ";
     cin >> f_name >> l_name;
     cout << f_name << " " << l_name;
+
     return 0;
 }
---
+++ Problems/balancedBinarySearchTree.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+};
+
+Node* createBinarySearchTree(Node* root, int data){
+    if (root == NULL){
+        Node* temp = new Node();
+        temp->data = data;
+        temp->left = NULL;
+        temp->right = NULL;
+        root = temp;
+    }
+    else if (data <= root->data){
+        root->left = createBinarySearchTree(root->left, data);
+    }
+    else{
+        root->right = createBinarySearchTree(root->right, data);
+    }
+    return root;
+}
+
+void treeTraversal(Node* root, vector<int> &v){
+    if(root == NULL) return;
+
+    treeTraversal(root->left, v);
+    v.push_back(root->data);
+    treeTraversal(root->right, v);
+
+}
+
+Node* balancedTree(vector<int> &v, int start, int end){
+    if(start > end) return NULL;
+    int mid = start + (end-start)/2;
+    Node* root = new Node();
+    root->data = v[mid];
+    root->left = balancedTree(v, start, mid-1);
+    root->right = balancedTree(v, mid+1, end);
+    return root;
+}
+
+void BFS(Node* root){
+    queue<Node*> q;
+    q.push(root);
+
+    while(!q.empty()){
+        Node* temp = q.front();
+        cout << temp->data;
+        if(temp->left != NULL){
+            q.push(temp->left);
+        }
+        if(temp->right != NULL){
+            q.push(temp->right);
+        }
+    }
+
+}
+
+
+int main(){
+
+    Node* root = NULL;
+    vector <int> v;
+    int a[] = {1,2,3,4,9,6,7};
+    int n = sizeof(a)/sizeof(a[0]);
+    
+    for(int i = 0; i < n ; i++){
+        root = createBinarySearchTree(root, a[i]);
+    }
+
+    //cout << "Tree: ";
+    treeTraversal(root, v);
+    BFS(root);
+    root = balancedTree(v, 0, v.size()-1);
+    
+    BFS(root);
+
+    return 0;
+
+}
